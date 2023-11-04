8a7ecd4f731fd3ad080c11d8020e3fa70143ace0
hemanthkumarreddybollavaram18 <149594249+hemanthkumarreddybollavaram18@users.noreply.github.com>
2023-11-05 00:11:23 +0530

    Add files via upload

---
+++ AnsibleSpringbootapp/AnsibleSpringboot (1)/AnsibleSpringboot/src/main/java/com/example/AnsibleSpringboot/AnsibleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.AnsibleSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleSpringbootApplication.class, args);
+	}
+
+}
---
+++ AnsibleSpringbootapp/AnsibleSpringboot (1)/AnsibleSpringboot/src/test/java/com/example/AnsibleSpringboot/AnsibleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnsibleSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsibleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
